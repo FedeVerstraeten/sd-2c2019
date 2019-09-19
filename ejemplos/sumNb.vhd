b8a05f90d0a232d2469c8878883aadf878587163
Federico Verstraeten <federico.verstraeten@gmail.com>
2019-09-19 10:51:34 -0300

    minor fixes

--- ejemplos/sumNb.vhd
+++ ejemplos/sumNb.vhd
@@ -1,52 +1,42 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
-use IEEE.numeric_std.all;
 
-entity sumNb_tb is
+entity sumNb is
+  generic(
+    N: natural := 4
+  );
+  port(
+    a: in std_logic_vector(N-1 downto 0);
+    b: in std_logic_vector(N-1 downto 0);
+    ci: in std_logic;
+    s: out std_logic_vector(N-1 downto 0);
+    co: out std_logic
+  );
+  
+  -- Mapeo de pines
+  attribute LOC: string;
+  attribute LOC of a: signal is "K17 K18 H18 G18";
+  attribute LOC of b: signal is "R17 N17 L13 L14";
+  attribute LOC of ci: signal is "B18";
+  attribute LOC of s: signal is "K14 K15 J15 J14";
+  attribute LOC of co: signal is "E17";
 end;
 
-architecture sumNb_tb_arq of sumNb_tb is
-
-  constant N_t: natural := 4;
-  
-  -- declaracion del componente a probar
-  component sumNb is
-    generic(
-      N: natural := 2
-    );
-    port(
-      a: in std_logic_vector(N-1 downto 0);
-      b: in std_logic_vector(N-1 downto 0);
-      ci: in std_logic;
-      s: out std_logic_vector(N-1 downto 0);
-      co: out std_logic
-    );
-  end component;
-
-  -- declaracion de senales de prueba
-  -- signal a_tb: std_logic_vector(N_t-1 downto 0) := "0011";
-  signal a_tb: std_logic_vector(N_t-1 downto 0) := std_logic_vector(to_unsigned(3, N_t));
---  signal b_tb: std_logic_vector(N_t-1 downto 0) := "0101";
-  signal b_tb: std_logic_vector(N_t-1 downto 0) := std_logic_vector(to_unsigned(5, N_t));
-  signal ci_tb: std_logic := '0';
-  signal s_tb: std_logic_vector(N_t-1 downto 0);
-  signal co_tb: std_logic;
-
+architecture sumNb_arq of sumNb is
+  signal c_aux: std_logic_vector(N downto 0);
 begin
-  a_tb <= std_logic_vector(to_unsigned(6, N_t)) after 100 ns;
-  b_tb <= std_logic_vector(to_unsigned(8, N_t)) after 200 ns;
-  ci_tb <= '1' after 300 ns;
+  c_aux(0) <= ci;
+  co <= c_aux(N);
   
-  DUT: sumNb
-    generic map(
-      N => N_t
-    )
-    port map(
-      a => a_tb,
-      b => b_tb,
-      ci => ci_tb,
-      s => s_tb,
-      co => co_tb
-    );
+  sumGen: for i in 0 to N-1 generate
+    sum1b_i: entity work.sum1b
+      port map(
+        a => a(i),
+        b => b(i),
+        ci => c_aux(i),
+        s => s(i),
+        co => c_aux(i+1)
+      );
+  end generate;
 
 end;
