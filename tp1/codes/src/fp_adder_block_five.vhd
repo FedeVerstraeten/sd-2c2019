bb19af1a6df896178529da1f9d9bb276d15a8287
Federico Verstraeten <federico.verstraeten@gmail.com>
2019-10-25 01:03:25 -0300

    update four & five blocks

--- tp1/codes/src/fp_adder_block_five.vhd
+++ tp1/codes/src/fp_adder_block_five.vhd
@@ -35,24 +35,29 @@ architecture beh of fp_adder_block_five is
   
 begin
 
-    sign_s <= sign_a when (sign_a = sign_b) else 
-          sign_b when ( ( sign_a /= sign_b ) AND flag_swap = '1') else
-          sign_a when ( ( sign_a /= sign_b ) AND flag_swap = '0' AND flag_s_twos_comp = '0') else
-          sign_b when ( ( sign_a /= sign_b ) AND flag_swap = '0' AND flag_s_twos_comp = '1' );
           
-    significand_add_one_with_carry <= ( (significand_add_one_with_carry( (FP_LEN-(FP_EXP+1)+1) downto 1 )'range =>'0' ) & (flag_s_add OR flag_r_add)  );
+    significand_add_one_with_carry <= ( (significand_add_one_with_carry( (FP_LEN-(FP_EXP+1)+1) downto 1 )'range =>'0' ) & (flag_s_add or flag_r_add));
           
     --25                        (1 &  24)                         25                
     significand_s_normalized_rounded_aux <= ( unsigned('0' & std_logic_vector(significand_s_normalized)) + significand_add_one_with_carry );    
                       
-    significand_s_normalized_rounded <= unsigned( '1' & significand_s_normalized_rounded_aux( (FP_LEN-(FP_EXP+1)) downto 1) ) when (significand_s_normalized_rounded_aux( FP_LEN - FP_EXP ) = '1') else
-                      significand_s_normalized_rounded_aux( ( FP_LEN - (FP_EXP+1) ) downto 0 );
+    significand_s_normalized_rounded <= unsigned( '1' & significand_s_normalized_rounded_aux( (FP_LEN-(FP_EXP+1)) downto 1) ) 
+      when (significand_s_normalized_rounded_aux( FP_LEN - FP_EXP ) = '1') 
+      else significand_s_normalized_rounded_aux( ( FP_LEN - (FP_EXP+1) ) downto 0 );
         
     exponent_add_one <= ( (exponent_add_one( (FP_EXP-2) downto 0 )'range =>'0' ) & '1' );
         
-    exponent_s <= ( exponent_a_plus_b + exponent_add_one )  when (significand_s_normalized_rounded_aux( FP_LEN - FP_EXP ) = '1') else
-              exponent_a_plus_b;
+    exponent_s <= ( exponent_a_plus_b + exponent_add_one )
+      when (significand_s_normalized_rounded_aux( FP_LEN - FP_EXP ) = '1')
+      else exponent_a_plus_b;
     
-    s_out <= ( sign_s & std_logic_vector(exponent_s) & std_logic_vector( significand_s_normalized_rounded( (FP_LEN -(FP_EXP+1)-1) downto 0) ) );
+    -- According to the sign rule table
+    sign_s <= sign_a when (sign_a = sign_b) else 
+          sign_b when ( ( sign_a /= sign_b ) and flag_swap = '1') else
+          sign_a when ( ( sign_a /= sign_b ) and flag_swap = '0' and flag_s_twos_comp = '0') else
+          sign_b when ( ( sign_a /= sign_b ) AND flag_swap = '0' AND flag_s_twos_comp = '1' );
+    
+    -- Concat sign + exp + mantissa
+    s_out <= ( sign_s & std_logic_vector(exponent_s) & std_logic_vector(significand_s_normalized_rounded((FP_LEN -(FP_EXP+1)-1) downto 0)));
           
 end beh;
--- tp1/codes/src/fp_adder_block_four.vhd
+++ tp1/codes/src/fp_adder_block_four.vhd
@@ -14,7 +14,6 @@ entity fp_adder_block_four is
     sign_b: in std_logic;
     exponent_a: in unsigned(FP_EXP-1 downto 0);
     significand_s: in unsigned(( FP_LEN - (FP_EXP+1) ) downto 0);
-    index: in unsigned(4 downto 0);
     carry_out: in std_logic; 
     flag_r: in std_logic;
     flag_g: in std_logic;
@@ -29,7 +28,33 @@ entity fp_adder_block_four is
 end fp_adder_block_four;
 
 architecture beh of fp_adder_block_four is
+  signal index: unsigned(4 downto 0):= (others => '0');
 begin
+  
+  index_def:process(sign_a,sign_b,significand_s,carry_out)
+    variable index_aux: integer :=0;
+  begin
+
+    if (sign_a /= sign_b) then
+
+      if (significand_s( FP_LEN-(FP_EXP+1) downto 0) /= (significand_s( FP_LEN-(FP_EXP+1) downto 0 )'range => '0')) then
+        while (significand_s((FP_LEN-(FP_EXP+1)) - index_aux)= '0') loop
+          index_aux := index_aux + 1;
+        end loop;       
+      else
+        index_aux:= FP_LEN-FP_EXP;
+      end if;     
+      index <= to_unsigned(index_aux,5);  
+
+    else
+      if ( carry_out = '0') then        
+        while (significand_s((FP_LEN-(FP_EXP+1)) - index_aux)= '0') loop
+          index_aux := index_aux + 1;
+        end loop;
+        index <= to_unsigned(index_aux,5);    
+      end if;
+    end if ;
+  end process index_def;
 
   significand_s_normalized <= ( carry_out & significand_s( (FP_LEN-(FP_EXP+1)) downto 1) ) 
   when ((sign_a xor sign_b)='0' and carry_out='1') 
@@ -40,7 +65,8 @@ begin
     else ( flag_g & (significand_s( (FP_LEN - (FP_EXP+1) - 1 ) downto 0)'range => '0') ) 
       when (to_integer(index) = (FP_LEN-(FP_EXP+1)+1) and flag_g = '1') 
     else ( others => '0');
-                
+
+  -- with carry and sign_a = sing_b
   exponent_a_plus_b <= ( exponent_a + to_unsigned(1,FP_EXP) )
   when ((sign_a xor sign_b)='0' and carry_out='1') 
     else ( exponent_a - index);     
@@ -52,9 +78,9 @@ begin
     else '0' 
       when to_integer(index) >= 2 ;
   
-  flag_s_add <= (flag_g OR significand_s(0) OR flag_s) 
+  flag_s_add <= (flag_g or significand_s(0) or flag_s) 
   when ((sign_a xor sign_b)='0' and carry_out='1') -- s:= or(g,r,s)
-    else (flag_r OR flag_s) 
+    else (flag_r or flag_s) 
       when to_integer(index) = 0 --s:= or(r,s)
     else '0' 
       when to_integer(index) >= 2 ;
