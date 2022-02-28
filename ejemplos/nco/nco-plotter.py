622207c04fce8c22ad1a063c9f01101685def998
Federico Verstraeten <federico.verstraeten@gmail.com>
2022-02-28 13:06:29 -0300

    testebench working and export data sin/cos to txt file

--- ejemplos/nco/nco-plotter.py
+++ ejemplos/nco/nco-plotter.py
@@ -1,22 +1,34 @@
 #!/usr/bin/env python3
 
 # Plot NCO output
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-FILE="./nco_output.txt"
-data_csv = pd.read_csv(FILE,sep=',',header=None)
-data_csv.reset_index(inplace=True)
-data_csv.columns=["x","sin"]
+FILE_SIN="./nco_sin_output.txt"
+FILE_COS="./nco_cos_output.txt"
 
-x=data_csv["x"]
-y=data_csv["sin"]
+data_sin = pd.read_csv(FILE_SIN,sep=',',header=None)
+data_sin.reset_index(inplace=True)
+data_sin.columns=["x","sin"]
 
-fig1,ax=plt.subplots()
-fig1.suptitle("NCO")
-ax.set(xlabel='x', ylabel='sin(x)')
-ax.grid()
-ax.plot(x,y,'-')
+data_cos = pd.read_csv(FILE_COS,sep=',',header=None)
+data_cos.reset_index(inplace=True)
+data_cos.columns=["x","cos"]
+
+x_sin=data_sin["x"]
+y_sin=data_sin["sin"]
+
+x_cos=data_cos["x"]
+y_cos=data_cos["cos"]
+
+fig,(ax_sin,ax_cos)=plt.subplots(2)
+
+fig.suptitle("NCO")
+ax_sin.set(xlabel='x', ylabel='sin(x)')
+ax_cos.set(xlabel='x', ylabel='cos(x)')
+ax_sin.grid()
+ax_cos.grid()
+ax_sin.plot(x_sin,y_sin,'b-')
+ax_cos.plot(x_cos,y_cos,'r-')
 
 plt.show()
--- ejemplos/nco/testbench/acum_fase_tb.vhd
+++ ejemplos/nco/testbench/acum_fase_tb.vhd
@@ -7,24 +7,28 @@ entity acum_fase_tb is
 end;
 
 architecture p of acum_fase_tb is
-	component acum_fase is
-		generic(
-			P: natural:= 5;
-			Q: natural:= 14;
-			N: natural:= 4 
-		);
-		port(
-			clk: in std_logic;
-			acum_reg: out std_logic_vector(N-1 downto 0)
-		);
-	end component acum_fase;
+    component acum_fase is
+        generic(
+            Q: natural:= 14;
+            N: natural:= 4;
+            INCREMENTO_W: natural:= 5
+        );
+        port(
+            clk: in std_logic;
+            incremento: in unsigned(INCREMENTO_W-1 downto 0);
+            acum_reg: out std_logic_vector(N-1 downto 0)
+        );
+    end component acum_fase;
 
-	constant N: natural:= 4;  -- cantidad de bits
-	signal clk: std_logic:= '0';
-	signal salida: std_logic_vector(N-1 downto 0);
+    constant N: natural:= 4;  -- cantidad de bits
+    constant P: natural:= 5;  -- paso en bits
+    signal clk: std_logic:= '0';
+    signal paso_prueba: unsigned(P-1 downto 0);
+    signal acum_reg_salida: std_logic_vector(N-1 downto 0);
 
 begin
 
-	clk <= not clk after 20 ns;
-	aa: acum_fase generic map(3, 14, N) port map(clk, salida);
+    clk <= not clk after 20 ns;
+    paso_prueba <= "00101";
+    aa: acum_fase generic map(14, N, P) port map(clk,paso_prueba,acum_reg_salida);
 end;
