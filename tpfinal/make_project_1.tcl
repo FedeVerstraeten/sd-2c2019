1ed97f5358dc133db502020981a93824446ce0e3
Federico Verstraeten <federico.verstraeten@gmail.com>
2022-03-03 13:42:21 -0300

    minor changes

--- tpfinal/make_project_1.tcl
+++ tpfinal/make_project_1.tcl
@@ -1,5 +1,5 @@
 # ==================================================================================================
-# make_project.tcl
+# make_project_1.tcl
 #
 # Simple script for creating a Vivado project from the project/ folder 
 # Based on Pavel Demin's red-pitaya-notes-master/ git project
@@ -7,6 +7,7 @@
 # Make sure the script is executed from redpitaya_guide/ folder
 #
 # by Anton Potocnik, 02.10.2016 - 14.12.2017
+# by Anton Potocnik, 01.01.2021 - 03.03.2021
 # ==================================================================================================
 
 set project_name "1_led_blink"
